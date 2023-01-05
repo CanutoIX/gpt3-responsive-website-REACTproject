94ab73590e7b4e2b45228ad4108185b6bf62a453
CanutoIX <caanutto@gmail.com>
2023-01-05 18:48:22 -0300

    Responsive Website React

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Article } from './article/Article'
+export { default as Brand } from './article/Brand'
+export { default as CTA } from './article/CTA'
+export { default as Feature } from './article/Feature'
+export { default as Navbar } from './article/Navbar'
