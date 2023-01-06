d38ad4457bea1f7d1d4a72aa242009f5d228c320
CanutoIX <caanutto@gmail.com>
2023-01-06 05:28:48 -0300

    Header + style

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css'
 const App = () => {
   return (
     <div className="App">
-        <div className="gradient_bg">
+        <div className="gradient__bg">
           <Navbar />
           <Header />
         </div>
