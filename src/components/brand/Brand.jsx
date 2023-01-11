8e4d259bebbed5e36daf532bd0ddbdc53a631bb8
CanutoIX <caanutto@gmail.com>
2023-01-11 06:51:15 -0300

    update

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './brand.css';
+import { google, slack, atlassian, dropbox, shopify } from './imports';
 
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className="gpt3__brand section__padding">
+      <div>
+        <img src={google} alt="google logo" />
+      </div>
+      <div>
+        <img src={slack} alt="slack logo" />
+      </div>
+      <div>
+        <img src={atlassian} alt="atlassian logo" />
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox logo" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify logo" />
+      </div>
+    </div>
   )
 }
 
